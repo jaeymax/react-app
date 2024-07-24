291c17f02f94b370a32683e32ab1d2388101a859
jaeymax <azagojunior2@gmail.com>
2024-07-24 12:23:54 +0000

    refactored the CareerStatus and VerifyAcount Page to use tailwindcss

--- src/pages/CareerStatus/CareerStatus.tsx
+++ src/pages/CareerStatus/CareerStatus.tsx
@@ -3,26 +3,26 @@ import './CareerStatus.css'
 
 const CareerStatus = () => {
   return (
-    <div className='career-status'>
+    <div className='flex flex-col h-full'>
       <Header/>
-      <div className="career-status-body">
-        <div className="career-status-content">
-            <h1>Where are you currently in your professional career?</h1>
-            <div>
+      <div className=" p-8 flex-1">
+        <div className=" flex flex-col gap-6 bg-blue-100 ">
+            <h1 className='font-bold text-2xl mb-6' >Where are you currently in your professional career?</h1>
+            <div className='border p-4 rounded-full text-center font-bold' >
               <h5>Still in school</h5>
             </div>
-            <div>
+            <div className='border p-4 rounded-full text-center font-bold'>
               <h5>Currently doing National Service</h5>
             </div>
-            <div>
+            <div className='border p-4 rounded-full text-center font-bold'>
               <h5>Done with National Service</h5>
             </div>
-            <div>
+            <div className='border p-4 rounded-full text-center font-bold'>
               <h5>
               Experienced Professional
               </h5>
             </div>
-            <button>Continue</button>
+            <button className='font-bold text-white bg-black p-4'>Continue</button>
         </div>
       </div>
     </div>
--- src/pages/VerifyAccount/VerifyAccount.tsx
+++ src/pages/VerifyAccount/VerifyAccount.tsx
@@ -3,21 +3,21 @@ import './VerifyAccount.css'
 
 const VerifyAccount = () => {
   return (
-    <div className="verify-account" >
+    <div className="flex flex-col h-full" >
       <Header/>
-      <div className="verify-account-body" >
+      <div className="flex-1 grid place-items-center text-center p-5" >
 
-        <div className="verify-account-content">
+        <div  className="verify-account-content border">
         <div className="verify-acccount-image">
 
         </div>
-        <div className="verify-account-details">
-            <h1>Verify your account</h1>
-            <p>A verification email will be sent to your email address provided. Click on the mail to verify your account.</p>
-            <div>
-              <h5>mic***@inpathgroup.africa</h5>
+        <div className="flex flex-col gap-8">
+            <h1 className="font-bold text-3xl">Verify your account</h1>
+            <p className="text-gray-500" >A verification email will be sent to your email address provided. Click on the mail to verify your account.</p>
+            <div className="border rounded-full p-3 w-[250px] bg-gray-100 mx-auto" >
+              <h5 className="font-bold text-gray-500 " >mic***@inpathgroup.africa</h5>
             </div>
-            <h6>Resend link</h6>
+            <h6 className="text-green-700 font-bold" >Resend link</h6>
         </div>
         </div>
       </div>
