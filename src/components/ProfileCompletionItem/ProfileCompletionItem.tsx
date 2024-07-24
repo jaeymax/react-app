52654c9547bc1bb9bc274e0a884d8eab6f3f4a16
jaeymax <azagojunior2@gmail.com>
2024-07-24 15:46:37 +0000

    refactored profileCompletion to use tailwindcss

--- src/components/ProfileCompletionItem/ProfileCompletionItem.tsx
+++ src/components/ProfileCompletionItem/ProfileCompletionItem.tsx
@@ -7,7 +7,7 @@ interface ProfileCompletionItemProps{
 
 const ProfileCompletionItem:React.FC<ProfileCompletionItemProps> = ({name}) => {
   return (
-    <div className="profile-completion-item" >
+    <div className="flex justify-between text-white font-bold p-5" >
       {name}
       <input type="checkbox" name="education" id="education" />
     </div>
